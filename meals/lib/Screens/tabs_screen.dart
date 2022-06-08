3ab5c993314a2ff8c0881eefb76bb72a6650135c
Yashaswa <yashaswashukla@gmail.com>
2022-06-08 16:20:56 +0530

    Added main drawer

--- meals/lib/Screens/tabs_screen.dart
+++ meals/lib/Screens/tabs_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+
 import './categories_screen.dart';
 import './favorites_screen.dart';
+import '../Widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -64,6 +66,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title']),
       ),
+      drawer: MainDrawer(),
       body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
