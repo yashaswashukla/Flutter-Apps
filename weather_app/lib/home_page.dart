cb59d9a40d44d2efe33efca06afa22a125a6e9dd
yashaswashukla <yashaswashukla@gmail.com>
2022-06-27 23:25:52 +0530

    Added assets

--- weather_app/lib/home_page.dart
+++ weather_app/lib/home_page.dart
@@ -5,8 +5,20 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(title: Text("Weather App")),
+      body: Container(
+        width: size.width,
+        height: size.height,
+        color: Colors.red,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+          ),
+        ),
+      ),
     );
   }
 }
