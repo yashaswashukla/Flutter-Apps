e4d5f58f5921b8f1e341529b9b9c5705df9b78e6
yashaswashukla <yashaswashukla@gmail.com>
2022-07-02 13:18:42 +0530

    Fixes

--- myshop/lib/Widgets/app_drawer.dart
+++ myshop/lib/Widgets/app_drawer.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/screens/auth_screen.dart';
 import 'package:provider/provider.dart';
 
 import '../Screens/user_product_screen.dart';
